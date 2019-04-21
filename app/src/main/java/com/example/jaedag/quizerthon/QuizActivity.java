baa32460f31b2fb3273b2071a4e25dcaf438f5b0
jd_addy <jaedagy@gmail.com>
2019-04-21 01:39:21 +0000

    Questions Update. Do not run until there are 100 questions!

--- app/src/main/java/com/example/jaedag/quizerthon/QuizActivity.java
+++ app/src/main/java/com/example/jaedag/quizerthon/QuizActivity.java
@@ -2,6 +2,7 @@ package com.example.jaedag.quizerthon;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,6 +12,9 @@ import android.widget.Toast;
 import android.os.Handler;
 
 import java.util.Random;
+
+import static com.example.jaedag.quizerthon.R.id.question_text_view;
+import static com.example.jaedag.quizerthon.R.id.textView;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -22,20 +26,15 @@ public class QuizActivity extends AppCompatActivity {
     private Button mNextButton;
     private TextView mQuestionTextView;
     public int quiz_results=0,count=0;
-    int min = 0;
-    int max = 22;
+    int min = 1;
+    int max = 101;
 
     //Randomise the question order with Random
     Random r = new Random();
-    private int mCurrentIndex  = r.nextInt(max - min + 1) + min;
+    private int mCurrentIndex  = r.nextInt(max - min + 1) + min;//min is inclusive max is exclusive1
 
     //This code contains the Question Bank
     private Question[] mQuestionBank = new Question[] {
-            new Question(R.string.question_oceans, true),
-            new Question(R.string.question_mideast, false),
-            new Question(R.string.question_africa, false),
-            new Question(R.string.question_americas, true),
-            new Question(R.string.question_asia, true),
             new Question(R.string.question_1,false),
             new Question(R.string.question_2,true),
             new Question(R.string.question_3,false),
@@ -63,7 +62,64 @@ public class QuizActivity extends AppCompatActivity {
             new Question(R.string.question_25,false),
             new Question(R.string.question_26,true),
             new Question(R.string.question_27,true),
-
+            new Question(R.string.question_28,true),
+            new Question(R.string.question_29,true),
+            new Question(R.string.question_30,false),
+            new Question(R.string.question_31,true),
+            new Question(R.string.question_32,false),
+            new Question(R.string.question_33,false),
+            new Question(R.string.question_34,true),
+            new Question(R.string.question_35,true),
+            new Question(R.string.question_36,true),
+            new Question(R.string.question_37,false),
+            new Question(R.string.question_38,true),
+            new Question(R.string.question_39,false),
+            new Question(R.string.question_40,false),
+            new Question(R.string.question_41,true),
+            new Question(R.string.question_42,false),
+            new Question(R.string.question_43,true),
+            new Question(R.string.question_44,false),
+            new Question(R.string.question_45,true),
+            new Question(R.string.question_46,true),
+            new Question(R.string.question_47,false),
+            new Question(R.string.question_48,false),
+            new Question(R.string.question_49,true),
+            new Question(R.string.question_50,false),
+            new Question(R.string.question_51,true),
+            new Question(R.string.question_52,false),
+            new Question(R.string.question_53,true),
+            new Question(R.string.question_54,true),
+            new Question(R.string.question_55,false),
+            new Question(R.string.question_56,false),
+            new Question(R.string.question_57,false),
+            new Question(R.string.question_58,true),
+            new Question(R.string.question_59,true),
+            new Question(R.string.question_60,false),
+            new Question(R.string.question_61,true),
+            new Question(R.string.question_62,true),
+            new Question(R.string.question_63,false),
+            new Question(R.string.question_64,false),
+            new Question(R.string.question_65,true),
+            new Question(R.string.question_66,true),
+            new Question(R.string.question_67,true),
+            new Question(R.string.question_68,false),
+            new Question(R.string.question_69,true),
+            new Question(R.string.question_70,true),
+            new Question(R.string.question_71,true),
+            new Question(R.string.question_72,true),
+            new Question(R.string.question_73,false),
+            new Question(R.string.question_74,true),
+            new Question(R.string.question_75,false),
+            new Question(R.string.question_76,false),
+            new Question(R.string.question_77,false),
+            new Question(R.string.question_78,true),
+            new Question(R.string.question_79,false),
+            new Question(R.string.question_80,false),
+            new Question(R.string.question_81, true),
+            new Question(R.string.question_82, false),
+            new Question(R.string.question_83, false),
+            new Question(R.string.question_84, true),
+            new Question(R.string.question_85, true),
 
 
 
@@ -126,7 +182,7 @@ public class QuizActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_quiz);
         quiz_results=0;
-        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+        mQuestionTextView = (TextView) findViewById(question_text_view);
         int question = mQuestionBank[mCurrentIndex].getTextResId();
         mQuestionTextView.setText(question);
 
@@ -173,7 +229,7 @@ public class QuizActivity extends AppCompatActivity {
     mNextButton.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+            mCurrentIndex = (mCurrentIndex + 3) % mQuestionBank.length;
             updateQuestion();
         }
     });
